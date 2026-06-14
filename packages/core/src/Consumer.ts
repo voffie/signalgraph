6064789f3434ef8aac64fcd817a3fc5cc05177da
voffie <viktorsoderborg@gmail.com>
2026-06-14 20:53:56 +0200

    feat: initial consumer implementation

---
+++ packages/core/src/Consumer.ts
@@ -0,0 +1,26 @@
+import { Effect } from "effect";
+import * as Message from "./Message.ts";
+
+export interface Consumer<
+  Name extends string,
+  TMessage extends Message.Message<string, unknown>
+> {
+  readonly _tag: "Consumer";
+  readonly name: Name;
+  readonly message: TMessage;
+  readonly handler: (payload: Message.PayloadOf<TMessage>) => Effect.Effect<void>;
+}
+
+export const make = <
+  const Name extends string,
+  TMessage extends Message.Message<string, unknown>
+>(options: {
+  readonly name: Name;
+  readonly message: TMessage;
+  readonly handler: (payload: Message.PayloadOf<TMessage>) => Effect.Effect<void>;
+}): Consumer<Name, TMessage> => ({
+  _tag: "Consumer",
+  name: options.name,
+  message: options.message,
+  handler: options.handler
+});

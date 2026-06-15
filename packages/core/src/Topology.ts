983b1904a41c6ddd4dc9eaa1a73acfa1bada697a
voffie <viktorsoderborg@gmail.com>
2026-06-15 15:28:03 +0200

    feat: initial topology implementation

---
+++ packages/core/src/Topology.ts
@@ -0,0 +1,49 @@
+import type * as Consumer from "./Consumer.ts";
+import type * as Message from "./Message.ts";
+
+export type AnyConsumer = Consumer.Consumer<string, Message.Message<string, unknown>>;
+export type AnyMessage = Message.Message<string, unknown>;
+
+export interface Topology<
+  TConsumers extends ReadonlyArray<AnyConsumer>
+> {
+  readonly _tag: "Topology";
+  readonly consumers: TConsumers;
+}
+
+export interface GraphEdge {
+  readonly message: AnyMessage;
+  readonly consumer: AnyConsumer;
+}
+
+export const make = <
+  const TConsumers extends ReadonlyArray<AnyConsumer>
+>(options: {
+  readonly consumers: TConsumers;
+}): Topology<TConsumers> => ({
+  _tag: "Topology",
+  consumers: options.consumers
+});
+
+export const consumers = <
+  TConsumers extends ReadonlyArray<AnyConsumer>
+>(
+  topology: Topology<TConsumers>
+): TConsumers => topology.consumers;
+
+export const messages = <
+  TConsumers extends ReadonlyArray<AnyConsumer>
+>(
+  topology: Topology<TConsumers>
+): ReadonlyArray<AnyMessage> =>
+  topology.consumers.map((consumer) => consumer.message);
+
+export const graph = <
+  TConsumers extends ReadonlyArray<AnyConsumer>
+>(
+  topology: Topology<TConsumers>
+): ReadonlyArray<GraphEdge> =>
+  topology.consumers.map((consumer) => ({
+    message: consumer.message,
+    consumer
+  }))

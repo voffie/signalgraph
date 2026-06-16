3f1c39e3f01ae7f9db9c19af8671fa3f8a40f9d0
voffie <viktorsoderborg@gmail.com>
2026-06-17 01:06:46 +0200

    feat: add index export for core project

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * as Message from "./Message.ts";
+export * as Consumer from "./Consumer.ts";
+export * as Topology from "./Topology.ts";
+export * as Messaging from "./Messaging.ts";
+export * from "./Driver.ts";

0f6efe1e9507e0b4bc1c3c279ed728fd63603fab
voffie <viktorsoderborg@gmail.com>
2026-07-15 20:17:25 +0200

    refactor: update project structure

---
+++ vitest.shared.ts
@@ -0,0 +1,37 @@
+import path from "node:path";
+import aliases from "vite-tsconfig-paths";
+import type { ViteUserConfig } from "vitest/config";
+
+const config: ViteUserConfig = {
+  oxc: {
+    target: "es2020"
+  },
+  plugins: [aliases()],
+  test: {
+    exclude: [
+      "**/node_modules/**"
+    ],
+    setupFiles: [path.join(__dirname, "vitest.setup.ts")],
+    sequence: {
+      concurrent: true
+    },
+    include: ["test/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["html"],
+      reportsDirectory: "coverage",
+      exclude: [
+        "node_modules/",
+        "dist/",
+        "build/",
+        "coverage/",
+        "**/*.d.ts",
+        "**/*.config.ts",
+        "**/vitest.setup.*",
+        "**/vitest.shared.*"
+      ]
+    }
+  }
+};
+
+export default config
